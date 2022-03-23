853507a5cce72661617b87ac795492605ea5891a
tanfiona <pillowtann@gmail.com>
2022-03-23 22:56:07 +0800

    Update experiments and hyperparameters

--- pdtb_kfolds.sh
+++ pdtb_kfolds.sh
@@ -16,9 +16,9 @@ echo "train and testing by folds"
 for i in `seq 1 $K`
 do
     echo "$i"
-    sudo CUDA_VISIBLE_DEVICES=1 /home/fiona/anaconda3/envs/torchgeom/bin/python3 run_case.py \
-    --task_name cola --train_file $SAVE_DIR/train_fold$i.csv --validation_file $SAVE_DIR/test_fold$i.csv \
-    --model_name_or_path bert-base-cased --output_dir outs/pdtb/folds/fold$i \
-    --overwrite_output_dir --do_train --do_eval --num_train_epochs 2 \
-    --per_device_train_batch_size 16 --per_device_eval_batch_size 32
+    sudo CUDA_VISIBLE_DEVICES=7 /home/fiona/anaconda3/envs/torchgeom/bin/python3 run_case.py \
+    --task_name cola --train_file $SAVE_DIR/train_fold$i.csv --do_train \
+    --validation_file $SAVE_DIR/test_fold$i.csv --do_eval \
+    --model_name_or_path bert-base-cased --output_dir outs/pdtb/folds/fold$i --overwrite_output_dir \
+    --num_train_epochs 10 --save_steps 50000 --per_device_train_batch_size 32 --per_device_eval_batch_size 32
 done
